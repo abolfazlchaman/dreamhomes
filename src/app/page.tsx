125c804648e25ae7fc704d0e60b50e4feb65f333
Abolfazl Chaman <88617114+abolfazlchaman@users.noreply.github.com>
2024-05-19 21:14:32 +0330

    added components and trademarks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./Components/Header/Header";
 import SearchBar from "./Components/SearchBar/SearchBar";
 import Featured from "./Components/Featured/Featured";
 import Trusted from "./Components/Trusted/Trusted";
+import Latest from "./Components/Latest/Latest";
+import Banner from "./Components/Banner/Banner";
+import Footer from "./Components/Footer/Footer";
 
 export default function Home() {
   return (
@@ -11,9 +14,25 @@ export default function Home() {
       <SearchBar />
       <Featured />
       <Trusted />
-      <span className="flex items-center justify-center  font-bold min-h-screen">
+      <Latest />
+      <Banner />
+      <Footer />
+      <span className="flex flex-col items-center justify-center text-center font-bold min-h-screen">
         <MdOutlineVilla size={"2.5rem"} />
-        dream homes
+        Dream Homes LLC. {new Date().getFullYear()}
+        <br />
+        &copy;All rights reserved.
+        <span className="font-light mt-12">
+          Developed and Maintained by <br />
+          <a
+            className="underline underline-offset-4"
+            href="http://abolfazlchaman.ir"
+            target="_blank"
+            rel="noopener noreferrer">
+            Abolfazl Chaman
+          </a>
+          .
+        </span>
       </span>
     </main>
   );
