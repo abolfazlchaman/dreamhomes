6eb528d7f1976101c0fededc3b158612720081bb
Abolfazl Chaman <88617114+abolfazlchaman@users.noreply.github.com>
2024-05-19 20:24:06 +0330

    added components to main instead of root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { MdOutlineVilla } from "react-icons/md";
+import Header from "./Components/Header/Header";
+import SearchBar from "./Components/SearchBar/SearchBar";
+import Featured from "./Components/Featured/Featured";
+import Trusted from "./Components/Trusted/Trusted";
 
 export default function Home() {
   return (
     <main>
+      <Header />
+      <SearchBar />
+      <Featured />
+      <Trusted />
       <span className="flex items-center justify-center  font-bold min-h-screen">
-        <MdOutlineVilla size={'2.5rem'} />
+        <MdOutlineVilla size={"2.5rem"} />
         dream homes
       </span>
     </main>
