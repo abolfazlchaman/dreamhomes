c1e9f85def80cb8f00ad9fb757bd24143a631fb0
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-05-14 22:13:45 +0330

    footer bottom updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,45 @@
-import { MdOutlineVilla } from "react-icons/md";
-import Header from "@/app/Components/Header/Header";
-import Featured from "@/app/Components/Featured/Featured";
-import Trusted from "@/app/Components/Trusted/Trusted";
-import Latest from "@/app/Components/Latest/Latest";
-import Banner from "@/app/Components/Banner/Banner";
-import Footer from "@/app/Components/Footer/Footer";
-import SearchBar from "@/app/Components/SearchBar/SearchBar";
+import { MdOutlineVilla } from 'react-icons/md';
+import Header from '@/app/Components/Header/Header';
+import Featured from '@/app/Components/Featured/Featured';
+import Trusted from '@/app/Components/Trusted/Trusted';
+import Latest from '@/app/Components/Latest/Latest';
+import Banner from '@/app/Components/Banner/Banner';
+import Footer from '@/app/Components/Footer/Footer';
+import SearchBar from '@/app/Components/SearchBar/SearchBar';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
       <Header />
-      <SearchBar />
-      <Featured />
-      <Trusted />
-      <Latest />
+      <section id='properties'>
+        <SearchBar />
+      </section>
+      <section id='properties'>
+        <Featured />
+      </section>
+      <section id='agencies'>
+        <Trusted />
+      </section>
+      <section id='about'>
+        <Latest />
+      </section>
       <Banner />
-      <Footer />
-      <span className="flex flex-col items-center justify-center text-center font-bold min-h-screen">
-        <MdOutlineVilla size={"2.5rem"} />
-        Dream Homes LLC. {new Date().getFullYear()}
+      <section id='footer'>
+        <Footer />
+      </section>
+      <span className='flex flex-col items-center justify-center text-center font-bold min-h-screen'>
+        <MdOutlineVilla size={'2.5rem'} />
+        DreamGem LLC. {new Date().getFullYear()}
         <br />
         &copy;All rights reserved.
-        <span className="font-light mt-12">
-          Developed and Maintained by <br />
-          <a
-            className="underline underline-offset-4"
-            href="http://abolfazlchaman.ir"
-            target="_blank"
-            rel="noopener noreferrer">
-            Abolfazl Chaman
-          </a>
+        <span className='font-light mt-12'>
+          Developed and Maintained by the <br />
+          <Link
+            className='underline underline-offset-4'
+            href='/'>
+            DreamGem Technichal Team
+          </Link>
           .
         </span>
       </span>
